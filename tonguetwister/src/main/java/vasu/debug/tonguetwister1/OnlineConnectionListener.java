3edf6f5a3a07b239592320f13c1022d87bfb92e8
Vasu Dev Mandal <code.vasu@gmail.com>
2021-03-05 12:44:41 +0530

    Delete tonguetwister/src directory

--- tonguetwister/src/main/java/vasu/debug/tonguetwister1/OnlineConnectionListener.java
+++
@@ -1,7 +0,0 @@
-package vasu.debug.tonguetwister1;
-
-public class OnlineConnectionListener {
-
-    //used for listening user/seller online connection status and informing them if they are offline
-
-}
